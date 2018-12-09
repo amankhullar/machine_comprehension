743f130f345659c86ec4e57069583e61cc5f63e5
Aman Khullar <amankhullar97@gmail.com>
2018-12-09 15:41:51 +0530

    BiDirectional (Bidaf) Attention Flow

---
+++ code/modules.py
@@ -0,0 +1,336 @@
+# Copyright 2018 Stanford University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This file contains some basic model components"""
+
+import tensorflow as tf
+from tensorflow.python.ops.rnn_cell import DropoutWrapper
+from tensorflow.python.ops import variable_scope as vs
+from tensorflow.python.ops import rnn_cell
+
+
+class RNNEncoder(object):
+    """
+    General-purpose module to encode a sequence using a RNN.
+    It feeds the input through a RNN and returns all the hidden states.
+
+    Note: In lecture 8, we talked about how you might use a RNN as an "encoder"
+    to get a single, fixed size vector representation of a sequence
+    (e.g. by taking element-wise max of hidden states).
+    Here, we're using the RNN as an "encoder" but we're not taking max;
+    we're just returning all the hidden states. The terminology "encoder"
+    still applies because we're getting a different "encoding" of each
+    position in the sequence, and we'll use the encodings downstream in the model.
+
+    This code uses a bidirectional GRU, but you could experiment with other types of RNN.
+    """
+
+    def __init__(self, hidden_size, keep_prob):
+        """
+        Inputs:
+          hidden_size: int. Hidden size of the RNN
+          keep_prob: Tensor containing a single scalar that is the keep probability (for dropout)
+        """
+        self.hidden_size = hidden_size
+        self.keep_prob = keep_prob
+        self.rnn_cell_fw = rnn_cell.GRUCell(self.hidden_size)
+        self.rnn_cell_fw = DropoutWrapper(self.rnn_cell_fw, input_keep_prob=self.keep_prob)
+        self.rnn_cell_bw = rnn_cell.GRUCell(self.hidden_size)
+        self.rnn_cell_bw = DropoutWrapper(self.rnn_cell_bw, input_keep_prob=self.keep_prob)
+
+    def build_graph(self, inputs, masks):
+        """
+        Inputs:
+          inputs: Tensor shape (batch_size, seq_len, input_size)
+          masks: Tensor shape (batch_size, seq_len).
+            Has 1s where there is real input, 0s where there's padding.
+            This is used to make sure tf.nn.bidirectional_dynamic_rnn doesn't iterate through masked steps.
+
+        Returns:
+          out: Tensor shape (batch_size, seq_len, hidden_size*2).
+            This is all hidden states (fw and bw hidden states are concatenated).
+        """
+        with vs.variable_scope("RNNEncoder"):
+            input_lens = tf.reduce_sum(masks, reduction_indices=1) # shape (batch_size)
+
+            # Note: fw_out and bw_out are the hidden states for every timestep.
+            # Each is shape (batch_size, seq_len, hidden_size).
+            (fw_out, bw_out), _ = tf.nn.bidirectional_dynamic_rnn(self.rnn_cell_fw, self.rnn_cell_bw, inputs, input_lens, dtype=tf.float32)
+
+            # Concatenate the forward and backward hidden states
+            out = tf.concat([fw_out, bw_out], 2)
+
+            # Apply dropout
+            out = tf.nn.dropout(out, self.keep_prob)
+
+            return out
+
+
+class SimpleSoftmaxLayer(object):
+    """
+    Module to take set of hidden states, (e.g. one for each context location),
+    and return probability distribution over those states.
+    """
+
+    def __init__(self):
+        pass
+
+    def build_graph(self, inputs, masks):
+        """
+        Applies one linear downprojection layer, then softmax.
+
+        Inputs:
+          inputs: Tensor shape (batch_size, seq_len, hidden_size)
+          masks: Tensor shape (batch_size, seq_len)
+            Has 1s where there is real input, 0s where there's padding.
+
+        Outputs:
+          logits: Tensor shape (batch_size, seq_len)
+            logits is the result of the downprojection layer, but it has -1e30
+            (i.e. very large negative number) in the padded locations
+          prob_dist: Tensor shape (batch_size, seq_len)
+            The result of taking softmax over logits.
+            This should have 0 in the padded locations, and the rest should sum to 1.
+        """
+        with vs.variable_scope("SimpleSoftmaxLayer"):
+
+            # Linear downprojection layer
+            logits = tf.contrib.layers.fully_connected(inputs, num_outputs=1, activation_fn=None) # shape (batch_size, seq_len, 1)
+            logits = tf.squeeze(logits, axis=[2]) # shape (batch_size, seq_len)
+
+            # Take softmax over sequence
+            masked_logits, prob_dist = masked_softmax(logits, masks, 1)
+
+            return masked_logits, prob_dist
+
+
+class BasicAttn(object):
+    """Module for basic attention.
+
+    Note: in this module we use the terminology of "keys" and "values" (see lectures).
+    In the terminology of "X attends to Y", "keys attend to values".
+
+    In the baseline model, the keys are the context hidden states
+    and the values are the question hidden states.
+
+    We choose to use general terminology of keys and values in this module
+    (rather than context and question) to avoid confusion if you reuse this
+    module with other inputs.
+    """
+
+    def __init__(self, keep_prob, key_vec_size, value_vec_size):
+        """
+        Inputs:
+          keep_prob: tensor containing a single scalar that is the keep probability (for dropout)
+          key_vec_size: size of the key vectors. int
+          value_vec_size: size of the value vectors. int
+        """
+        self.keep_prob = keep_prob
+        self.key_vec_size = key_vec_size
+        self.value_vec_size = value_vec_size
+
+    def build_graph(self, values, values_mask, keys):
+        """
+        Keys attend to values.
+        For each key, return an attention distribution and an attention output vector.
+
+        Inputs:
+          values: Tensor shape (batch_size, num_values, value_vec_size).
+          values_mask: Tensor shape (batch_size, num_values).
+            1s where there's real input, 0s where there's padding
+          keys: Tensor shape (batch_size, num_keys, value_vec_size)
+
+        Outputs:
+          attn_dist: Tensor shape (batch_size, num_keys, num_values).
+            For each key, the distribution should sum to 1,
+            and should be 0 in the value locations that correspond to padding.
+          output: Tensor shape (batch_size, num_keys, hidden_size).
+            This is the attention output; the weighted sum of the values
+            (using the attention distribution as weights).
+        """
+        with vs.variable_scope("BasicAttn"):
+
+            # Calculate attention distribution
+            values_t = tf.transpose(values, perm=[0, 2, 1]) # (batch_size, value_vec_size, num_values)
+            attn_logits = tf.matmul(keys, values_t) # shape (batch_size, num_keys, num_values)
+            attn_logits_mask = tf.expand_dims(values_mask, 1) # shape (batch_size, 1, num_values)
+            _, attn_dist = masked_softmax(attn_logits, attn_logits_mask, 2) # shape (batch_size, num_keys, num_values). take softmax over values
+
+            # Use attention distribution to take weighted sum of values
+            output = tf.matmul(attn_dist, values) # shape (batch_size, num_keys, value_vec_size)
+
+            # Apply dropout
+            output = tf.nn.dropout(output, self.keep_prob)
+
+            return attn_dist, output
+
+
+def masked_softmax(logits, mask, dim):
+    """
+    Takes masked softmax over given dimension of logits.
+
+    Inputs:
+      logits: Numpy array. We want to take softmax over dimension dim.
+      mask: Numpy array of same shape as logits.
+        Has 1s where there's real data in logits, 0 where there's padding
+      dim: int. dimension over which to take softmax
+
+    Returns:
+      masked_logits: Numpy array same shape as logits.
+        This is the same as logits, but with 1e30 subtracted
+        (i.e. very large negative number) in the padding locations.
+      prob_dist: Numpy array same shape as logits.
+        The result of taking softmax over masked_logits in given dimension.
+        Should be 0 in padding locations.
+        Should sum to 1 over given dimension.
+    """
+    exp_mask = (1 - tf.cast(mask, 'float')) * (-1e30) # -large where there's padding, 0 elsewhere
+    masked_logits = tf.add(logits, exp_mask) # where there's padding, set logits to -large
+    prob_dist = tf.nn.softmax(masked_logits, dim)
+    return masked_logits, prob_dist
+
+class BidirectionalAttn(object):
+    """Self implemented module for Bidirectional attention flow of the hidden states of the context and the attention"""
+
+    def __init__(self, hidden_size, keep_prob, key_vec_size, value_vec_size):
+        self.hidden_size = hidden_size
+        self.keep_prob = keep_prob
+        self.key_vec_size = key_vec_size
+        self.value_vec_size = value_vec_size
+
+    def build_graph(self, values, values_mask, keys, keys_mask):
+        with vs.variable_scope("BidirectionalAttn"):
+            weight_similar = tf.get_variable(name = "weight_similar", shape = [6 * self.hidden_size, self.key_vec_size], initializer = tf.contrib.layers.xavier_initializer())
+            reshape(values, shape = [2*h, -1])
+            reshape(values_mask, shape = [2*h, -1])
+            reshape(keys, shape = [2*h, -1])
+            reshape(keys_mask, shape = [2*h, -1])
+            similarity_matrix = tf.matmul(tf.transpose(weight_similar), tf.concat([values, keys, values * keys], 0) )
+
+def similarity_matrix(keys, values):
+        with vs.variable_scope("similarity_matrix"):
+            w_keys = tf.get_variable("w_keys", shape = [keys.get_shape().as_list()[-1]], dtype = tf.float32)
+            dot_keys = tf.tensordot(keys, w_keys, axes = [[2], [0]])
+
+            w_values = tf.get_variable("w_values", shape = [values.get_shape().as_list()[-1]], dtype = tf.float32)
+            dot_values = tf.tensordot(values, w_values, axes = [[2], [0]])
+
+            dot_w = tf.get_variable("dot_w", shape = keys.get_shape().as_list()[-1], dtype = tf.float32)
+
+            keys_dots = keys * tf.expand_dims(tf.expand_dims(dot_w,0), 0)
+            dot_logits = tf.matmul(keys_dots, keys, transpose_b = True)
+
+            return dot_logits + tf.expand_dims(dot_values, 1) + tf.expand_dims(dot_keys, 2)
+
+class BiDirectionalAttention(object):
+    """ This is similar to the implementation of CS224n's submission. I am trying to improve on the code.
+    """
+
+    def __init__(self, keep_prob, key_vec_size, value_vec_size, hidden_size):
+        self.keep_prob = keep_prob
+        self.key_vec_size = key_vec_size
+        self.value_vec_size = value_vec_size
+        self.hidden_size = hidden_size
+
+    def build_graph(self, keys, keys_mask, values, values_mask):
+        """
+        The Dimensions of various tensors are as follows :
+        keys : batch_size * context_size * hidden_size
+        value : batch_size * questions_size * hidden_size
+        keys_mask = batch_size * context_size
+        value_mask = batch_size * question_size
+        """
+        context_size = keys.get_shape().as_list()[1]
+        question_size = values.get_shape().as_list()[1]
+        
+        keys_mask_new = tf.expand_dims(keys_mask, axis = -1)
+        values_mask_new = tf.expand_dims(values_mask, axis = 1)
+        keys_values_mask = tf.cast(keys_mask_new, tf.bool) & tf.cast(values_mask_new, tf.bool) #size of key_value_mask = [batch_size, context_size, question_size]
+        
+        with vs.variable_scope("BiDirectionalAttention"):
+            S = similarity_matrix(keys, values)
+            
+        with vs.variable_scope("Context2Question"):
+            _, a_c2q = masked_softmax(S, keys_values_mask, dim = -1)
+            attention_c2q = tf.matmul(a_c2q, values)
+
+        with vs.variable_scope("Question2Context"):
+            m = tf.reduce_max(S, axis = -1)
+            _, b = masked_softmax(m, keys_mask, dim = -1)
+            b = tf.expand_dims(b, axis = -1)
+            #attention_q2c = tf.matmul(b, keys)
+            attention_q2c = tf.reduce_sum(b * keys, axis = 1)
+            attention_q2c = f.tile(tf.expand_dims(attention_q2c, axis = 1), [1, context_size, 1])
+
+        with vs.variable_scope("Concat_result"):
+            attention_result = tf.concat([keys, attention_c2q, keys * attention_c2q, keys * attention_q2c], -1)
+        return attention_result
+
+class Bidaf(object):
+    """
+    This is my third attempt at writing the BiDirectional Attention flow layer
+    """
+
+    def __init__(self, keep_prob, key_vec_size, value_vec_size):
+        self.keep_prob = keep_prob
+        self.key_vec_size = key_vec_size
+        self.value_vec_size = value_vec_size
+
+    def build_graph(self, keys, keys_mask, values, values_mask):
+
+        # keys : [batch_size, context_size, 2*hidden_size]
+        # values : [batch_size, question_size, 2*hidden_size]
+        with vs.variable_scope("Bidaf"):
+            w_sim1 = tf.get_variable("w_sim1", shape = [self.key_vec_size, ], initializer = tf.contrib.layers.xavier_initializer(), dtype = tf.float32)
+            w_sim2 = tf.get_variable("w_sim2", shape = [self.key_vec_size, ], initializer = tf.contrib.layers.xavier_initializer(), dtype = tf.float32)
+            w_sim3 = tf.get_variable("w_sim3", shape = [self.key_vec_size, ], initializer = tf.contrib.layers.xavier_initializer(), dtype = tf.float32)
+    
+            wk = tf.tensordot(keys, w_sim1, [[2], [0]])    #(batch_size, context_size)
+            wv = tf.tensordot(values, w_sim2, [[2], [0]])  #(batch_size, question_size)
+            wok = tf.multiply(keys, w_sim3)                #(batch_size, context_size, 2*hidden_size)
+            v_transpose = tf.transpose(values, perm = [0, 2, 1]) #(batch_size, 2*hidden_size, question_size)
+            wkov = tf.matmul(wok, v_transpose)            #(batch_size, context_size, question_size)
+
+            S = tf.expand_dims(wk, 2) + tf.expand_dims(wv, 1) + wkov #(batch_size, context_size, question_size)
+
+            #Context2Question
+            attn_mask_values = tf.expand_dims(values_mask, 1)    #(batch_size, 1, question_size)
+            _, attn_dist = masked_softmax(S, attn_mask_values, 2) #(batch_size, context_size, question_size)
+
+            Ut = tf.matmul(attn_dist, values)              #(batch_size, context_size, 2*hidden_size)
+            Ut = tf.transpose(Ut, perm = [0, 2, 1])        #(batch_size, 2*hidden_size, context_size)
+
+            #Question2Context
+            Smax_col = tf.reduce_max(S, axis = 2)          #(batch_size, context_size)
+            _, b = masked_softmax(Smax_col, keys_mask, 1)  #(batch_size, context_size)
+            b = tf.expand_dims(b, 1)                       #(batch_size, 1, context_size)
+            ht = tf.matmul(b, keys)                        #(batch_size, 1, 2*hidden_size)
+            ht = tf.squeeze(ht)                            #(batch_size, 2*hidden_size)
+
+            #Output
+            batch_size = tf.shape(ht)[0]                   #[batch_size]
+            dim1_size = tf.shape(ht)[1]                    #[2*hidden_size]
+            dim2_size = tf.shape(Ut)[2]                    #[context_size]
+            ht = tf.reshape(ht, (-1, ))
+            Ht = tf.reshape(tf.tile(ht, [dim2_size]), (batch_size, dim1_size, dim2_size)) #(batch_size, 2*hidden_size, context_size)
+
+            keys_H = tf.transpose(keys, perm = [0, 2, 1])       #(batch_size, 2*hidden_size, context_size)
+            HoUt = tf.multiply(keys_H, Ut)                 #(batch_size, 2*hidden_size, context_size)
+            HoHt = tf.multiply(keys_H, Ht)                 #(batch_size, 2*hidden_size, context_size)
+
+            G = tf.concat([keys_H, Ut, HoUt, HoHt], axis = 1)   #(batch_size, 8*hidden_size, context_size)
+            output = tf.transpose(G, perm = [0, 2, 1])     #(batch_size, context_size, 8*hidden_size)
+            output = tf.nn.dropout(output, self.keep_prob)
+
+            return attn_dist, b, output
